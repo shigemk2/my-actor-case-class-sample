0a689d5ccb42f073b2175fbe7fdee8ed848f78f8
shigemk2 <i.am.shige@gmail.com>
2016-09-04 02:24:07 +0900

    Add MyActor

--- src/main/scala/com/example/MyActor.scala
+++ src/main/scala/com/example/MyActor.scala
@@ -1,5 +1,18 @@
 package com.example
 
-class MyActor {
+import akka.actor.Actor
+import akka.event.Logging
 
+case class MyCaseClass(prop1: Int, prop2: String)
+
+class MyActor extends Actor {
+  val log = Logging(context.system, this)
+
+  def receive = {
+    case MyCaseClass(prop1, prop2) => {
+      log.info("prop1: %d, prop2: %s" format (prop1, prop2))
+    }
+    case _ => {
+    }
+  }
 }
