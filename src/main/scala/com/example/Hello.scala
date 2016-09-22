8af394b71d94c27f8d1a4748df4f7c93ad0de2c6
shigemk2 <i.am.shige@gmail.com>
2016-09-22 19:35:35 +0900

    Add comments

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -6,10 +6,13 @@ object Hello {
   def main(args: Array[String]): Unit = {
     val system = ActorSystem("mySystem")
 
+    // アクターの生成
     val props = Props[MyActor]
     val actor = system.actorOf(props, name = "myActor")
 
+    // ケースクラスを渡す
     val msg1 = new MyCaseClass(123, "abc")
+    // ただの文字列
     val msg2 = "test"
 
     for(i <- 1 to 5) {
